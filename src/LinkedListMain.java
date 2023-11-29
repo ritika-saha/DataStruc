636ea8919b8d788f3f41a69aaf607261e9f2823b
ritika-saha <ritika882001@gmail.com>
2023-11-30 00:22:44 +0530

    cleanup

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -1,9 +1,5 @@
 public class LinkedListMain {
     public static void main(String[] args) throws Exception {
-     LinkedList<Integer> list=new LinkedList<>();
-     list.add(56);
-     list.add(30);
-     list.add(70);
-     list.display();
+     
     }
 }
