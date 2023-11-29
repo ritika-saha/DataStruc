1b3323cb63dc2cfe25a3271f6eeb0bf6748d62b3
ritika-saha <ritika882001@gmail.com>
2023-11-30 00:17:16 +0530

    cleanup

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -1,10 +1,5 @@
 public class LinkedListMain {
     public static void main(String[] args) throws Exception {
-        //uc2- branch uc2_addFrontLL -adding nodes in front
-        LinkedList<Integer> list=new LinkedList<>();
-        list.addFront(70);
-        list.addFront(30);
-        list.addFront(56);
-        list.display();
+     
     }
 }
